f9550b9ec07b4ff2d95aa6f08fe000eb262c8477
Claire <claire.hw.cho@gmail.com>
2023-02-27 23:38:23 -0800

    change cookie

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
     cookie: {
-        maxAge: 300000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
+        maxAge: 300000
+        // httpOnly: true,
+        // secure: false,
+        // sameSite: 'strict',
     },
     resave: false,
     saveUninitialized: true,
